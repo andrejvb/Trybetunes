0926f471205ce04db978dd39e7accd88f7443283
andrejvb <andre.jvb@gmail.com>
2023-01-12 22:36:37 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    searchItem: '',
+  };
+
   render() {
+    const { searchItem } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">Search</div>
+        <form>
+          <label htmlFor="artistName">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ ({ target }) => this.setState({
+                searchItem: target.value,
+              }) }
+            />
+          </label>
+          <button
+            disabled={ searchItem.length < 2 }
+            data-testid="search-artist-button"
+            type="button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </>
     );
   }
