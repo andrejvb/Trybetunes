4fb35022b6193b790b66eab7dd293fd59bc229a5
andrejvb <andre.jvb@gmail.com>
2023-01-16 01:15:34 -0300

    requisito 08 lint

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Loading from './Loading';
+// import Loading from './Loading';
 import { addSong } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
